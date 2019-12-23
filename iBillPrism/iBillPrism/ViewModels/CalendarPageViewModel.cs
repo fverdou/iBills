d5439d29df6e3950cf48d31116c5a9f550007ec3
Foteini Verdou <foteiniverdou@outlook.com>
2019-12-23 16:57:49 +0200

    Benchmark before refactoring.

--- iBillPrism/iBillPrism/ViewModels/CalendarPageViewModel.cs
+++ iBillPrism/iBillPrism/ViewModels/CalendarPageViewModel.cs
@@ -16,90 +16,87 @@ namespace iBillPrism.ViewModels
 {
     public class CalendarPageViewModel : ViewModelBase
     {
-
-        //public DelegateCommand ButtonAddBillClicked { get; }
+        public event EventHandler IsActiveChanged;
+        public DelegateCommand RefreshCommand { get; }
         public DelegateCommand<Bill> EditButtonCommand { get; }
         public DelegateCommand<Bill> DeleteButtonCommand { get; }
-        //public ObservableRangeCollection<Bill> ListOfBills { get; }
         public ObservableRangeCollection<BillsGroup> ListOfBills { get; }
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, RaiseIsActiveChanged);
+        }
+        private void RaiseIsActiveChanged()
+        {
+            IsActiveChanged?.Invoke(this, EventArgs.Empty);
+            RefreshCommand.Execute();
+        }
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+            set => SetProperty(ref _isRefreshing, value);
+        }
 
         public CalendarPageViewModel(INavigationService navigationService, IRepository repository, IPageDialogService dialogService)
             : base(navigationService)
         {
             _pageDialogService = dialogService;
             _repository = repository;
-            //ButtonAddBillClicked = new DelegateCommand(AddBillCommand);
             EditButtonCommand = new DelegateCommand<Bill>(o => EditButtonTap(o));
-            DeleteButtonCommand = new DelegateCommand<Bill>(o => DeleteButtonTap(o));
+            DeleteButtonCommand = new DelegateCommand<Bill>(async o => await DeleteButtonTap(o));
+            RefreshCommand = new DelegateCommand(async () => await RefreshData());
 
-            //ListOfBills = new ObservableRangeCollection<Bill>();
             ListOfBills = new ObservableRangeCollection<BillsGroup>();
         }
-        public async override void OnNavigatedTo(INavigationParameters parameters)
+        public override void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
+            RefreshCommand.Execute();
+        }
+        private async Task RefreshData()
+        {
+            try
+            {
+                IsRefreshing = true;
 
-            var data = await _repository.GetAllBills();
-            //ListOfBills.ReplaceRange(data.OrderBy(x => x.DueDate));
-            ListOfBills.ReplaceRange(
-                data
-                .GroupBy(x => x.DueDate)
-                .OrderBy(x => x.Key)
-                .Select(x => new BillsGroup(x.Key, x.ToList())));
+                var data = await _repository.GetAllBills();
+                ListOfBills.ReplaceRange(
+                    data
+                    .GroupBy(x => x.DueDate)
+                    .OrderBy(x => x.Key)
+                    .Select(x => new BillsGroup(x.Key, x.ToList())));
+            }
+            catch (Exception exception)
+            {
+                await _pageDialogService.DisplayAlertAsync("Error !!", exception.Message, "Ok");
+            }
+            finally
+            {
+                IsRefreshing = false;
+            }
         }
 
-        private async void DeleteButtonTap(Bill b)
+        private async Task DeleteButtonTap(Bill b)
         {
             bool answer = await _pageDialogService.DisplayAlertAsync(null, "Are you sure you want to delete this bill?", "Yes", "No");
             if (answer)
             {
                 await _repository.RemoveBill(b);
-                var data = await _repository.GetAllBills();
-                //ListOfBills.ReplaceRange(data.OrderBy(x => x.DueDate));
-                ListOfBills.ReplaceRange(
-                data
-                .GroupBy(x => x.DueDate)
-                .OrderBy(x => x.Key)
-                .Select(x => new BillsGroup(x.Key, x.ToList())));
+                await RefreshData();
             }
         }
-
-        //async void AddBillCommand()
-        //{         
-        //    await NavigationService.NavigateAsync("DataEntryPage");
-        //}
 
         async void EditButtonTap(Bill b)
         {
             var parameters = new NavigationParameters();
             parameters.Add("bill", b);
-            //new navigationparameters { { "bill", b } }
             await NavigationService.NavigateAsync("DataEntryPage", parameters);
-
-            //var action = await _pageDialogService.DisplayActionSheetAsync("", "Cancel", null, "Edit bill", "Delete bill");
-
-            //switch (action)
-            //{
-            //    case "Edit bill":
-            //        {
-            //            var parameters = new NavigationParameters();
-            //            parameters.Add("bill", b);
-            //            //new NavigationParameters { { "bill", b } }
-            //            await NavigationService.NavigateAsync("DataEntryPage", parameters);
-            //        }
-            //        break;
-            //    case "Delete bill":
-
-            //        await _repository.Remove(b); 
-            //        var data = await _repository.GetAll();
-            //        ListOfBills.ReplaceRange(data.OrderBy(x => x.DueDate));
-
-            //        break;
-            //}
 
         }
 
         private readonly IRepository _repository;
         private readonly IPageDialogService _pageDialogService;
+        private bool _isActive;
+        private bool _isRefreshing = false;
     }
 }
